2e4636782ca43bbed55830a1761091eb4f44acea
Jeremy Cocks <jeremy.cocks@forgerock.com>
2021-09-16 14:04:16 +0100

    4:Write multiply Test

--- calculator_test.go
+++ calculator_test.go
@@ -22,3 +22,11 @@ func TestSubtract(t *testing.T) {
 		t.Errorf("want %f, got %f", want, got)
 	}
 }
+func TestMultiply(t *testing.T) {
+	t.Parallel()
+	var want float64 = 16
+	got := calculator.Multiply(4, 4)
+	if want != got {
+		t.Errorf("want %f, got %f", want, got)
+	}
+}
